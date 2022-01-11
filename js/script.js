556ba57e5c73e64bdbd5fddb90f9b1e44897f8cb
wish837 <ImWisdomH@gmail.com>
2022-01-11 15:18:10 +0900

    Edit script.js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,44 @@
 const main = document.querySelector("#main");
 const test = document.querySelector("#test");
 const result = document.querySelector("#result");
-
 const endPoint = 12;
-const select = [];
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function calResult() {
-    var pointArray = [
-        { name: 'gryffindor', value: 0, key: 0 },
-        { name: 'hufflepuff', value: 0, key: 1 },
-        { name: 'ravenclaw', value: 0, key: 2 },
-        { name: 'slytherin', value: 0, key: 3 }, 
-    ]
-    for(let i = 0; i < endPoint; i++) {
-        var target = qnaList[i].a[select[i]];
-        for (let j = 0; j < target.length; j++) {
-            for (let k = 0; k < pointArray.length; k++) {
-                if(target.type[j] === pointArray[k].name) {
-                    pointArray[k].value += 1;
-                }
-            }
-        }
-    }
+    console.log(select);
+    var result = select.indexOf(Math.max(...select));
+    return result;
 }
 
-function goTest() {
-    main.style.WebkitAnimation = "fadeOut 1s";
-    main.style.animation = "fadeOut 1s";
+function setResult(){
+    let point = calResult();
+    const resultName = document.querySelector('#resultName');
+    resultName.innerHTML = resultList[point].name;
+
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = 'asset/img/' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+
+    const resultDesc = document.querySelector('#resultDesc');
+    resultDesc.innerHTML = resultList[point].desc;
+}
+
+function goResult() {
+    test.style.WebkitAnimation = "fadeOut 1s";
+    test.style.animation = "fadeOut 1s";
     setTimeout(() => {
-        test.style.WebkitAnimation = "fadeIn 1s";
-        test.style.animation = "fadeIn 1s";
+        result.style.WebkitAnimation = "fadeIn 1s";
+        result.style.animation = "fadeIn 1s";
         setTimeout(() => {
-            main.style.display = "none";
-            test.style.display = "block";
+            test.style.display = "none";
+            result.style.display = "block";
         }, 450)
-        let qIdx = 0;
-        goNext(qIdx);
-    }, 450);
+    })
+    setResult();
 }
 
 function addAnswer(answerText, qIdx, idx) {
@@ -50,12 +52,15 @@ function addAnswer(answerText, qIdx, idx) {
         var children = document.querySelectorAll('.answer-list');
         for (let i = 0; i < children.length; i++) {
             children[i].disabled = true;
-            children[i].style.WebkitAnimation = "fadeOut 1s";
-            children[i].style.animation = "fadeOut 1s";
+            children[i].style.WebkitAnimation = "fadeOut 0.5s";
+            children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(() => {
-            select[qIdx] = idx;
-            for(let i = 0; i < children.length; i++) {
+            var target = qnaList[qIdx].a[idx].type;
+            for (let i = 0; i < target.length; i++) {
+                select[target[i]] += 1;
+            }
+            for (let i = 0; i < children.length; i++) {
                 children[i].style.display = 'none';
             }
             goNext(++qIdx);
@@ -65,7 +70,7 @@ function addAnswer(answerText, qIdx, idx) {
 
 function goNext(qIdx) {
     var mainImg = document.querySelector("#mainImg");
-    if (qIdx+1 === endPoint) {
+    if (qIdx === endPoint) {
         mainImg.style.WebkitAnimation = "fadeOut 1s";
         mainImg.style.animation = "fadeOut 1s";
         setTimeout(() => {
@@ -83,15 +88,18 @@ function goNext(qIdx) {
     status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
-function goResult() {
-    test.style.WebkitAnimation = "fadeOut 1s";
-    test.style.animation = "fadeOut 1s";
+
+function goTest() {
+    main.style.WebkitAnimation = "fadeOut 1s";
+    main.style.animation = "fadeOut 1s";
     setTimeout(() => {
-        result.style.WebkitAnimation = "fadeIn 1s";
-        result.style.animation = "fadeIn 1s";
+        test.style.WebkitAnimation = "fadeIn 1s";
+        test.style.animation = "fadeIn 1s";
         setTimeout(() => {
-            test.style.display = "none";
-            result.style.display = "block";
+            main.style.display = "none";
+            test.style.display = "block";
         }, 450)
-    });
+        let qIdx = 0;
+        goNext(qIdx);
+    }, 450);
 }
